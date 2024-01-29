3f1034902c9b58017ed1a4e500ddb0446792d6f4
manhnvan <manh12344444444@gmail.com>
2024-01-29 08:59:41 +0700

    fix: build with CGO_ENABLED=1 to run with sqlite

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 CGO_ENABLED=1 go build -a -installsuffix cgo -o monitor ./cmd/monitor
-CGO_ENABLED=0 go build -a -installsuffix cgo -o api ./cmd/api
+CGO_ENABLED=1 go build -a -installsuffix cgo -o api ./cmd/api
